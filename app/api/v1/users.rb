b9808e6e72c8d609599dc200037a27a05670eedd
Michael Liou <michael78123@gmail.com>
2016-05-05 19:04:28 -0700

    API: post user works fine in terminal, but doesn't work in browser(405)

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -6,6 +6,38 @@ module V1
       get do
         User.all
       end
+
+      desc "Create a new user"
+
+      params do
+        requires :name, type: String
+        requires :email, type: String
+        requires :password, type: String
+      end
+
+      post do
+        user = User.new
+        user.email = params[:email]
+        user.password = params[:password]
+        user.name = params[:name]
+        user.save!
+
+        # return { name: user.name }
+        # User.create({
+        #   name: params[:name],
+        #   email: params[:email],
+        #   password: params[:password]
+        #   })
+      end
+
+      desc 'test'
+      params do
+        requires :answer, type: Integer
+      end
+
+      get '/testing' do
+        {"answer" => params[:answer]}
+      end
     end
   end
 end
